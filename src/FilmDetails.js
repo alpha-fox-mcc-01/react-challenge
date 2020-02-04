866b505bfaac799527a2fc666919be5d5f6dbb2c
vaniairnanda <vaniairnanda@gmail.com>
2020-02-04 16:02:13 +0700

    details done

--- src/FilmDetails.js
+++ src/FilmDetails.js
@@ -1,13 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import {  useParams } from "react-router-dom"
 import useFetcher from './useFetcher'
 import imageList from './helpers/images'
 
 export function FilmDetails(props) {
-  const [images, setImages] = useState([])
   const { id } = useParams()
-   const [loading, data, error] = useFetcher('/films/' + id)
-
+  const [loading, data, error] = useFetcher('/films/' + id)
+  console.log(imageList, 'ini imagelist')
    const upperStyles = {
      paddingLeft : '25px'
    }
@@ -16,12 +15,16 @@ export function FilmDetails(props) {
    }
 
    const imageStyle = {
-     width: '500px'
+     width: '500px',
+     maxHeight: '100%'
    }
    const descContainer = {
      justifyContent: 'center',
      padding: '2rem'
    }
+
+   if (loading) return <p>Loading....</p>
+   if (error) return <p>Oops... An Error Occured</p>
   return (
     <div className="w-full"> 
     <div style={upperStyles} className="flex-col bg-white shadow">
@@ -31,7 +34,10 @@ export function FilmDetails(props) {
     </div>
     <div className="flex mb-4 flex-wrap">
       <div style={descContainer} className="w-1/2 h-12 flex">
-        <img src="" alt="film-poster" />
+      { data.title ? imageList.map( image => {
+            if (image.title.toLowerCase() === data.title.toLowerCase()) {
+              return <img src={image.url} alt='filmposter' key={image.title} />
+      }}) : '' }
       </div>
       <div style={descContainer} className="w-1/2 h-12">
       <p>{data.description}</p>
